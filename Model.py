bab0b3aaf4639fc7310eb630f64b7cb91a92b3df
feng-master <554286238@qq.com>
2018-02-03 20:22:27 +0800

    数据预处理部分修改
    今晚跑数据试试

--- Model.py
+++ Model.py
@@ -6,11 +6,13 @@ import jieba
 import jieba.analyse
 from numpy import *
 import numpy as np
-
+from sklearn.linear_model import LinearRegression
 from Box import Box
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn import preprocessing
 from sklearn.neural_network import MLPRegressor
+from  sklearn.linear_model import ElasticNet
+
 # 缩放参数,用于降低问题详细内容中关键词的权重(相对于标题来说)
 SHRINK_PARM = 0.5
 
@@ -18,7 +20,11 @@ SHRINK_PARM = 0.5
 def learn():
     db = DBHelper.get_instance()
 
-    Model = MLPRegressor(solver='lbfgs', alpha=1e-5,hidden_layer_sizes=(5, 2), random_state=1)
+    X_normalize = preprocessing.Normalizer()
+    Y_normalize = preprocessing.MinMaxScaler()
+
+    # Model = MLPRegressor(solver='lbfgs', alpha=1e-40,hidden_layer_sizes=(5, 2), random_state=1)
+    Model = ElasticNet(alpha=1e-40, tol=1e-38,max_iter=10000)
 
     # ----------初始化--------------#
 
@@ -29,7 +35,7 @@ def learn():
 
     # ----------初始化--------------#
 
-    questions = db.get_all_questions(mlimit=10)  # 训练总问题数
+    questions = db.get_all_questions(mlimit=5)  # 训练总问题数
 
     index = 0
 
@@ -39,19 +45,34 @@ def learn():
         title_tags = jieba.analyse.extract_tags(question['title'], topK=5, withWeight=True)
         content_tag = jieba.analyse.extract_tags(question['content'], topK=8, withWeight=True)
 
+        title_tags.sort(key=lambda d:d[1])
+
         tags = title_tags
 
         # 提取出关键词列表,用于简单去重
         tags_words = [t[0] for t in tags]
 
-        for ct in content_tag:
-            if not (ct[0] in tags_words):
-                tags.append([ct[0], ct[1] * SHRINK_PARM])
+        content_tag.sort(key=lambda d:d[1])
+
+        _dta=0
+        while len(tags)<8:
+            if len(content_tag) > _dta:
+                if not (content_tag[_dta][0] in tags_words):
+                    tags.append([content_tag[_dta][0],content_tag[_dta][1]*SHRINK_PARM])
+                    _dta+=1
+                else:
+                    _dta+=1
+            else:
+                tags.append([0,0])
+
 
         # 关键词ID列表
         question_key_word_ids = [db.get_key_word_id(t[0]) for t in tags]
         # 关键词权重列表,作为特征值用于模型训练
         question_key_word_weights = [t[1] for t in tags]
+
+        # 权重每个+1,不然会出现小于0的权重,导致相乘的时候分数缩小
+        question_key_word_weights = [t + 1 for t in question_key_word_weights]
 
         if len(question_key_word_ids) < 8:
             #     特征值不足
@@ -69,6 +90,8 @@ def learn():
             for content in o:
                 answers_id_list.append(content[0])
 
+
+        # 答案ID列表去重
         answers_id_list = set(answers_id_list)
 
         # -----------------提取含有问题关键词的答案列表-----------------#
@@ -100,16 +123,16 @@ def learn():
                         ans_key_word_info.append([0, 0])
             if ans_key_word_info:
                 # 答案关键词ID列表
-                ans_key_word_ids = [info[0] for info in ans_key_word_info]
+                ans_key_word_ids = [_info[0] for _info in ans_key_word_info]
             else:
                 ans_key_word_ids = []
 
-            for i in range(len(ans_key_word_ids)):
-                if ans_key_word_ids[i] in question_key_word_ids:
+            for i in range(len(question_key_word_ids)):
+                if question_key_word_ids[i] in ans_key_word_ids:
                     # 如果某关键词ID在问题关键词和答案关键词中都有,那么命中,计算特征值(用相应权值相乘
                     key_word_feature.append(
-                        question_key_word_weights[question_key_word_ids.index(ans_key_word_ids[i])] *
-                        ans_key_word_info[i][1])
+                        question_key_word_weights[i] *
+                        (ans_key_word_info[ans_key_word_ids.index(question_key_word_ids[i])][1]+1))
                 else:
                     # 如果没有,直接置为0
                     key_word_feature.append(0)
@@ -117,39 +140,50 @@ def learn():
             # 除了点赞数.点赞数用于评分(输出)
 
             ft = [ans['create_time'], ans['update_time'], ans['comment_count']]
+
             ft.extend(key_word_feature)
 
-
+            # 计算分数
 
             vp = ans['voteup_count']
             if vp == 0:
                 vp = 1
-            temp_y = 0
+            temp_y = 1
+            cy = 0
             for i in key_word_feature:
-                temp_y *= i * vp
+                if i != 0:
+                    temp_y *= i * vp
+                    cy += 1
 
-
+            temp_y = temp_y ** cy
 
             answers_id_score_feature_list.append([_id, temp_y, ft])
-            # answers_feature.append(ft)
-            #
-            #
-            #
-            # Y.append(temp_y)
-            # ALL_X.append(answers_feature)
-            # ALL_QID.append(question['question_id'])
-            # ALL_Y.append(Y)
 
         # 选出最高分前100
         answers_id_score_feature_list.sort(key=lambda d: d[1], reverse=True)
+
+        each_x=[_i[2] for _i in answers_id_score_feature_list]
+        each_y = [_i[1] for _i in answers_id_score_feature_list]
+
+        each_x=preprocessing.MinMaxScaler().fit_transform(each_x)
+        each_y=preprocessing.MinMaxScaler().fit_transform(array(each_y).reshape(-1, 1))
+
+        print('-------------')
+        for _i in range(3):
+            print('id:',answers_id_score_feature_list[_i][0])
+            print('score',each_y[_i])
+            print('feature',each_x[_i])
+
+
+        print('-------------')
 
         _len = len(answers_id_score_feature_list)
         if _len > 100:
             _len = 100
 
         for i in range(_len):
-            ALL_X.append(answers_id_score_feature_list[i][2])
-            ALL_Y.append(answers_id_score_feature_list[i][1])
+            ALL_X.append(each_x[i])
+            ALL_Y.append(each_y[i])
             ALL_QID.append(question['question_id'])
 
         print('处理问题%d' % (index))
@@ -157,7 +191,12 @@ def learn():
 
     print('开始训练')
     print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
-    Model.fit(preprocessing.scale(ALL_X), preprocessing.scale(ALL_Y))
+    X_normalize = preprocessing.MinMaxScaler().fit(array(ALL_X))
+    Y_normalize = preprocessing.MinMaxScaler().fit(array(ALL_Y).reshape(-1, 1))
+    XXXXXX = ALL_X
+    YYYYYY = ALL_Y
+    Model.fit(XXXXXX, YYYYYY)
+
     print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
     print("训练一次")
     # -----------------计算每个答案的特征值和标准评分-----------------#
@@ -168,7 +207,7 @@ def learn():
 
     new_ques = '游戏行业现状'
 
-    tags = jieba.analyse.extract_tags(new_ques, topK=8,withWeight=True)
+    tags = jieba.analyse.extract_tags(new_ques, topK=8, withWeight=True)
 
     print("测试问题关键词:")
     for t in tags:
@@ -178,7 +217,7 @@ def learn():
 
     # 问题关键词ID列表
     ques_key_word_ids = [db.get_key_word_id(t[0]) for t in tags]
-    ques_key_word_weights=[t[1] for t in tags]
+    ques_key_word_weights = [t[1] for t in tags]
 
     ans_id_list = []
 
@@ -188,13 +227,31 @@ def learn():
         for content in o:
             ans_id_list.append(content[0])
 
+
+
+    # list_all_ans=[]
+    # for wid in ques_key_word_ids:
+    #     o = db.get_answers_by_key_word_id(wid)
+    #     list_all_ans.append([content[0] for content in o])
+
+    # for ans_l in list_all_ans:
+    #     if len(list_all_ans)==0:
+    #         ans_id_list=ans_l
+    #     else:
+    #         t_ans_id_list=list(set(ans_l).intersection(set(ans_id_list)))
+    #         if len(t_ans_id_list)==0:
+    #             break
+
+
+    for need in range(8-len(ques_key_word_ids)):
+        ques_key_word_ids.append(0)
+        ques_key_word_weights.append(0)
+
     ans_id_list = set(ans_id_list)
 
     score_list = []
 
     test_ALL_X = []
-
-    _list = []
 
     for _id in ans_id_list:
 
@@ -213,36 +270,35 @@ def learn():
         # 答案关键词ID列表
         ans_key_word_ids = [_info[0] for _info in ans_key_word_info]
 
+        _i = 0
 
-        _i=0
-
-        for i in range(len(ans_key_word_ids)):
-            if ans_key_word_ids[i] in ques_key_word_ids:
+        for i in range(len(ques_key_word_ids)):
+            if ques_key_word_ids[i] in ans_key_word_ids:
                 # 如果某关键词ID在问题关键词和答案关键词中都有,那么命中,计算特征值(用相应权值相乘
-                key_word_feature.append(ques_key_word_weights[ques_key_word_ids.index(ans_key_word_ids[i])] * ans_key_word_info[i][1])
-                _i+=1
+                key_word_feature.append(
+                    ques_key_word_weights[i] * (ans_key_word_info[ans_key_word_ids.index(ques_key_word_ids[i])][1])+1)
+                _i += 1
             else:
                 # 如果没有,直接置为0
                 key_word_feature.append(0)
-
-        _list.append([_id,_i])
 
         feature = [ans['create_time'], ans['update_time'], ans['comment_count']]
         feature.extend(key_word_feature)
 
         test_ALL_X.append(feature)
 
-    _list.sort(key=lambda d:d[1],reverse=True)
 
-    s = Model.predict(preprocessing.scale(test_ALL_X))
+    s = Model.predict(preprocessing.MinMaxScaler().fit_transform(test_ALL_X))
     score_list = np.vstack((array(list(ans_id_list)), array(s)))
 
     sl = list(score_list.transpose().tolist())
 
     sl.sort(key=lambda d: d[1], reverse=True)
 
-    for u in sl[0:20]:
-        ki=db.get_answer_key_word_info_by_id(u[0])
+    for u in sl[0:120]:
+        ki = db.get_answer_key_word_info_by_id(u[0])
+
+        print(db.get_answer_by_id(int(u[0]))['voteup_count'])
 
         for kii in ki:
             print(db.get_key_word_by_id(kii[0]))
@@ -250,5 +306,6 @@ def learn():
         print('内容:\n')
         print(db.get_answer_by_id(int(u[0]))['content'])
         print('\n')
+
 
 learn()
